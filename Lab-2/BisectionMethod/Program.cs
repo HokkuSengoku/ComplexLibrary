8877e4231ffc371add9a6f49bef345f9b1cf60c6
aleksandr_tsvetkov <darksouls2make@gmail.com>
2022-10-24 21:40:49 +0400

    Fix changes

--- Lab-2/BisectionMethod/Program.cs
+++ Lab-2/BisectionMethod/Program.cs
@@ -14,11 +14,24 @@ namespace BisectionMethod
             var a = 0.0;
             var b = 2.0;
             var eps = 0.0001;
+            var halfAB = 0.0;
+            var productInPoints = 0.0;
+            var countOfIteration= 0;
+            
+            do
+            {
+                countOfIteration++;
+                halfAB = (a + b) / 2;
+                productInPoints = Function(halfAB) * Function(a);
 
-            var y = Function((b - a) / 2);
-
-            Console.WriteLine("Корень уравнения равен " + y);
-            Console.WriteLine("Количество итераций равно " + 1);
+                if (productInPoints > 0)
+                    a = halfAB;
+                else if (productInPoints < 0)
+                    b = halfAB;
+            } while (Math.Abs(a - b) > eps);
+            
+            Console.WriteLine("Корень уравнения равен " + halfAB);
+            Console.WriteLine("Количество итераций равно " + countOfIteration);
         }
     }
 }
