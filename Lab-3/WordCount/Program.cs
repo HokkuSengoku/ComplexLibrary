de66f19d567b66827c5071b798b1f9a69df6bcce
aleksandr_tsvetkov <darksouls2make@gmail.com>
2022-11-08 23:01:08 +0400

    Final Version

--- Lab-3/WordCount/Program.cs
+++ Lab-3/WordCount/Program.cs
@@ -1,6 +1,7 @@
 namespace WordCount
 {
     using System;
+    using System.Collections.Generic;
     using System.IO;
     using System.Text;
 
@@ -9,28 +10,77 @@ namespace WordCount
         private static void Main(string[] args)
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var letterDictionary = new Dictionary<string, string>();
 
-            string file1Text = File.ReadAllText(@"Data\Беда_одна_не_ходит.txt");
+            var oldWinEncoding = Encoding.GetEncoding(1251);
+            var winBytes = File.ReadAllBytes(@"Data\Начало.txt");
+            var file2Text = oldWinEncoding.GetString(winBytes);
+
+            string file1Path = @"Data\Беда_одна_не_ходит.txt";
+            string file3Path = @"Data\Хэппи_Энд.txt";
 
             int wordCount = 0;
 
-            wordCount += CalculateWordCount(file1Text);
+            wordCount += CalculateWordCount(ReadFile(file1Path), letterDictionary);
+            wordCount += CalculateWordCount(file2Text, letterDictionary);
+            wordCount += CalculateWordCount(ReadFile(file3Path), letterDictionary);
 
             Console.WriteLine("Count of words in story files: {0}", wordCount);
         }
 
-        private static int CalculateWordCount(string text)
+        private static int CalculateWordCount(string text, Dictionary<string, string> letterDictionary)
         {
             int wordCount = 0;
-            foreach (char ch in text)
+            string result;
+            char[] delimiterChars = { ' ', ',', '.', ':', '\t' };
+            var textSplit = text.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var letter in textSplit)
             {
-                if (char.IsLetterOrDigit(ch))
+                for (var i = 0; i < letter.Length; i++)
                 {
+                    if (char.IsLetter(letter[i]))
+                    {
+                        sb.Append(char.ToLower(letter[i]));
+                    }
+                }
+
+                result = sb.ToString();
+
+                if (!letterDictionary.ContainsKey(result) && result != string.Empty)
+                {
+                    letterDictionary[result] = result;
                     wordCount++;
                 }
+
+                sb.Clear();
             }
 
             return wordCount;
+        }
+
+        private static string ReadFile(string path)
+        {
+            var sb = new StringBuilder();
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        sb.Append(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+            return sb.ToString();
         }
     }
 }
