272507201f8d82205719ad5b806797f17ec00aec
aleksandr_tsvetkov <darksouls2make@gmail.com>
2022-12-04 22:24:04 +0400

    Final Version

--- Lab-4/Lib/ComplexNumbers/TestComplexLibrary/Program.cs
+++ Lab-4/Lib/ComplexNumbers/TestComplexLibrary/Program.cs
@@ -1,38 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Numerics;
+﻿using System.Security.Cryptography;
 using ComplexActions;
 
+// Complex numbers
+var a = new ComplexNumberType(1.0, 2.0);
+var b = new ComplexNumberType(2.0, 5.0);
+var c = new ComplexNumberType(15.0, 82.3);
+int n = 3;
+Console.WriteLine($"Complex number a: {a.ToString()}\n" +
+                  $"Complex number b: {b.ToString()}\n" +
+                  $"Complex number c: {c.ToString()}\n" +
+                  $"Degree indicator n: {n}\n");
 
+// The sum of two complex numbers
+Console.WriteLine($"Sum of two complex numbers a and b: " +
+                  $"{ComplexDo.ComplexSum(a, b)}\n");
 
-ComplexDo.ComplexSum(new ComplexNumberType(1.0, 2.0), new ComplexNumberType(1.0, 2.0), new ComplexNumberType(1.0, 2.0));
-Console.WriteLine(ComplexDo.ComplexSum(new ComplexNumberType(1.0, 2.0), new ComplexNumberType(1.0, 2.0), new ComplexNumberType(1.0, 2.0)).ToString());
+// The sum of the set of complex numbers
+Console.WriteLine($"The sum of the set of complex numbers a, b, c: " +
+                  $"{ComplexDo.ComplexSum(a, b, c)}\n");
 
-/*Console.WriteLine(ComplexDo.ComplexMultiplication(new ComplexNumberType(2.0, 3.0), new ComplexNumberType(-1.0, 1.0)).ToString());
-Console.WriteLine(new ComplexNumberType(2.0, -5.0).ToString());
-Console.WriteLine(new ComplexNumberType(2.0, 1.0).ToString());
-Console.WriteLine(ComplexDo.ComplexDiv(new ComplexNumberType(3.0, -1.0), new ComplexNumberType(-5.0, 2.0)).ToString());*/
+// The difference of two complex numbers.
+Console.WriteLine($"The difference of two complex numbers a, b: " +
+                  $"{ComplexDo.ComplexSubstract(a, b)}\n");
 
-Console.WriteLine(Complex.Multiply(new Complex(3.0, -1.0), new Complex(-5.0, 2.0)));
-Console.WriteLine(ComplexDo.ComplexMultiplication(new ComplexNumberType(3.0, -1.0), new ComplexNumberType(-5.0, 2.0)).ToString());
-Console.WriteLine(Complex.Multiply(new Complex(-13.0, 11.0), new Complex(-5.0, 2.0)));
+// Quotient of two complex numbers.
+Console.WriteLine($"The quotient of two complex numbers a, c: " +
+                  $"{ComplexDo.ComplexDiv(a, c)}\n");
 
-Console.WriteLine(ComplexDo.ComplexMultiplication(new ComplexNumberType(3.0, -1.0), new ComplexNumberType(-5.0, 2.0), new ComplexNumberType(-5.0, 2.0)).ToString());
-Console.WriteLine(ComplexDo.ComplexAbs(new ComplexNumberType(0.5, Math.Sqrt(3) / 2)));
-Console.WriteLine(ComplexDo.ComplexArg(new ComplexNumberType(0.5, Math.Sqrt(3) / 2)));
-double abs = ComplexDo.ComplexAbs(new ComplexNumberType(1.0, Math.Sqrt(3.0)));
-double arg = ComplexDo.ComplexArg(new ComplexNumberType(1.0, Math.Sqrt(3.0)));
-Console.WriteLine($"{abs}      {arg}");
+// The product of two complex numbers.
+Console.WriteLine($"The product of two complex numbers b, c: " +
+                  $"{ComplexDo.ComplexMultiplication(b, c)}\n");
 
-Console.WriteLine(ComplexDo.ComplexPowTrigForm(new ComplexNumberType(2.0, 5.0), 3).ToString());
+// The product of a set of complex numbers.
+Console.WriteLine($"The product of a set of complex numbers a, b, c: " +
+                  $"{ComplexDo.ComplexMultiplication(a, b, c)}\n");
 
-ComplexDo.GetNComplexRoots(new ComplexNumberType(2.0, 5.0), 5);
-Console.WriteLine((new ComplexNumberType(3.0,2.0),new ComplexNumberType(3.0, 2.0)));
+// The argument of a complex number b.
+Console.WriteLine($"The argument of a complex number b: {ComplexDo.ComplexArg(b)}\n");
 
-var a = new ComplexNumberType(3.0, 5.0);
+// The module of a complex number b.
+Console.WriteLine($"The module of a complex number b: {ComplexDo.ComplexAbs(b)}\n");
+
+// A complex number b to the power of N
+Console.WriteLine($"A complex number b to the power of N: {ComplexDo.ComplexPowTrigForm(b, n)}\n");
+
+// The root of the 3rd degree of the complex number b.
+Console.WriteLine($"The root of the 3rd degree of the complex number b: ");
+ComplexDo.GetNComplexRoots(b, n);
+Console.WriteLine();
+
+// Output of a complex number B as a string
+Console.WriteLine($"Output of a complex number B as a string: {b.ToString()}\n");
+
+// The Equals method for complex numbers.
 object d = new ComplexNumberType(5.2, 3.6);
-Console.WriteLine(a.Equals(new ComplexNumberType(3.0, 6.0)));
-Console.WriteLine(d.Equals(d));
-Console.WriteLine(a.GetHashCode());
-Console.WriteLine(new ComplexNumberType(default, default));
+Console.WriteLine($"Checking via equals of the d object. Result: {d.Equals(d)}");
 
+// Comparison of two instances of complex number objects.
+Console.WriteLine($"Comparison of two instances of complex number objects A and D. Result: {a.Equals(d)}");
+
+// Getting the hash code of a complex number.
+Console.WriteLine($"Getting the hash code of a complex number A. Result: {a.GetHashCode()}");
