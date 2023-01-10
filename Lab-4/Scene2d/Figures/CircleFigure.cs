67fe7e5434a9ac95a6a35f225d423b7792902da7
aleksandr_tsvetkov <darksouls2make@gmail.com>
2023-01-11 00:13:43 +0400

    Added a method for finding a flowing rectangle.

--- Lab-4/Scene2d/Figures/CircleFigure.cs
+++ Lab-4/Scene2d/Figures/CircleFigure.cs
@@ -2,7 +2,7 @@ namespace Scene2d.Figures
 {
     using System.Drawing;
 
-    public class CircleFigure // : IFigure
+    public class CircleFigure : IFigure
     {
         private ScenePoint _center;
         private double _radius;
@@ -11,6 +11,35 @@ namespace Scene2d.Figures
         {
             _center = center;
             _radius = radius;
+        }
+
+        public SceneRectangle CalculateCircumscribingRectangle()
+        {
+            var x1 = _center.X - _radius;
+            var y1 = _center.Y - _radius;
+            var x2 = _center.X + _radius;
+            var y2 = _center.Y + _radius;
+
+            SceneRectangle aFlowingRectangle = default;
+            aFlowingRectangle.Vertex1 = new ScenePoint(x1, y1);
+            aFlowingRectangle.Vertex2 = new ScenePoint(x2, y2);
+
+            return aFlowingRectangle;
+        }
+
+        public void Move(ScenePoint vector)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Rotate(double angle)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Reflect(ReflectOrientation orientation)
+        {
+            throw new System.NotImplementedException();
         }
 
         public void Draw(ScenePoint origin, Graphics drawing)
@@ -25,6 +54,11 @@ namespace Scene2d.Figures
                     (int)(_radius * 2),
                     (int)(_radius * 2));
             }
+        }
+
+        public object Clone()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
--- Lab-4/Scene2d/Figures/PolygonFigure.cs
+++ Lab-4/Scene2d/Figures/PolygonFigure.cs
@@ -3,13 +3,66 @@ namespace Scene2d.Figures
     using System.Drawing;
     using System.Linq;
 
-    public class PolygonFigure // : IFigure
+    public class PolygonFigure : IFigure
     {
         private ScenePoint[] _points;
 
         public PolygonFigure(ScenePoint[] points)
         {
             _points = points;
+        }
+
+        public SceneRectangle CalculateCircumscribingRectangle()
+        {
+            double x1, y1, x2, y2;
+            x1 = _points[0].X;
+            x2 = _points[0].X;
+            y1 = _points[0].Y;
+            y2 = _points[0].Y;
+
+            for (var i = 1; i < _points.Length; i++)
+            {
+                if (_points[i].X < x1)
+                {
+                    x1 = _points[i].X;
+                }
+
+                if (_points[i].Y < y1)
+                {
+                    y1 = _points[i].Y;
+                }
+
+                if (_points[i].X > x2)
+                {
+                    x2 = _points[i].X;
+                }
+
+                if (_points[i].Y > y2)
+                {
+                    y2 = _points[i].Y;
+                }
+            }
+
+            SceneRectangle aFlowingRectangle = default;
+            aFlowingRectangle.Vertex1 = new ScenePoint(x1, y1);
+            aFlowingRectangle.Vertex2 = new ScenePoint(x2, y2);
+
+            return aFlowingRectangle;
+        }
+
+        public void Move(ScenePoint vector)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Rotate(double angle)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Reflect(ReflectOrientation orientation)
+        {
+            throw new System.NotImplementedException();
         }
 
         public void Draw(ScenePoint origin, Graphics drawing)
@@ -31,6 +84,11 @@ namespace Scene2d.Figures
                         (float)(secondPoint.Y - origin.Y));
                 }
             }
+        }
+
+        public object Clone()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
