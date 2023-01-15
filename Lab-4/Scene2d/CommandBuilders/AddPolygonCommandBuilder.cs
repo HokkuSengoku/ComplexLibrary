8fc909d955a3b5cc037553509dc1af1711b8cb70
aleksandr_tsvetkov <darksouls2make@gmail.com>
2023-01-15 19:38:04 +0400

    The final implementation of the polygon addition command.

--- Lab-4/Scene2d/CommandBuilders/AddPolygonCommandBuilder.cs
+++ Lab-4/Scene2d/CommandBuilders/AddPolygonCommandBuilder.cs
@@ -1,6 +1,8 @@
 ﻿namespace Scene2d.CommandBuilders
 {
     using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
     using System.Text.RegularExpressions;
     using Scene2d.Commands;
     using Scene2d.Exceptions;
@@ -8,40 +10,70 @@
 
     public class AddPolygonCommandBuilder : ICommandBuilder
     {
-        private static readonly Regex RecognizeRegex = new Regex(@"((add\spolygon)\s((\w+||[-])*)\n(\s(add\spoint\s\([+-]?\d*,\s?[+-]?\d*\))\n){3,}end\spolygon)");
+        private static readonly Regex RecognizeRegex = new Regex(@"((add\spolygon)\s((\w+||[-])*))|((\s(add\spoint\s\([+-]?\d*,\s?[+-]?\d*\))))|(end\spolygon)");
+        private static readonly Regex AddPointRegex = new Regex(@"((add\spoint\s\([+-]?\d*,\s?[+-]?\d*\)))");
+        private static readonly Regex CommandRegex = new Regex(@"((add\spolygon)\s((\w+||[-])*))");
+        private static readonly Regex EndPolygonRegex = new Regex(@"(end\spolygon)");
 
         /* Should be set in AppendLine method */
         private IFigure _polygon;
 
         /* Should be set in AppendLine method */
         private string _name;
+        private bool _isCommonReady;
+        private List<double> _allCoordinates = new List<double>();
 
         public bool IsCommandReady
         {
             get
             {
-                return true;
+                return _isCommonReady;
             }
         }
 
         public void AppendLine(string line)
         {
             var separators = new char[] { ' ', '(', ')', ',' };
-            var match = RecognizeRegex.Match(@line);
+            var match = RecognizeRegex.Match(line);
+            _isCommonReady = false;
             if (match.Value != string.Empty)
             {
                 var command = match.Value.Split(separators, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-                var coordinates = GetCoordinates(command);
-                ScenePoint[] points = new ScenePoint[coordinates.Length / 2];
-                _name = command[2];
-                _polygon = default;
-
-                for (var i = 0; i < points.Length; i++)
+                if (CommandRegex.Match(match.Value).Success)
                 {
-                    points[i] = new ScenePoint { X = coordinates[i], Y = coordinates[i + 1] };
+                    _name = command[2];
+                    _isCommonReady = false;
                 }
 
-                _polygon = new PolygonFigure(points);
+                if (AddPointRegex.Match(match.Value).Success)
+                {
+                    var coordinates = GetCoordinates(command);
+                    foreach (var item in coordinates)
+                    {
+                        _allCoordinates.Add(item);
+                    }
+
+                    _isCommonReady = false;
+                }
+
+                if (EndPolygonRegex.Match(match.Value).Success)
+                {
+                    ScenePoint[] points = new ScenePoint[_allCoordinates.Count / 2];
+                    List<ScenePoint> pointss = new List<ScenePoint>();
+                    for (var i = 0; i < _allCoordinates.Count; i += 2)
+                    {
+                        pointss.Add(new ScenePoint { X = _allCoordinates[i], Y = _allCoordinates[i + 1] });
+                    }
+
+                    for (var i = 0; i < points.Length; i++)
+                    {
+                        points[i] = pointss[i];
+                    }
+
+                    _polygon = default;
+                    _polygon = new PolygonFigure(points);
+                    _isCommonReady = true;
+                }
             }
             else
             {
@@ -51,10 +83,10 @@
 
         public double[] GetCoordinates(string[] command)
         {
-            var coordinates = new double[(command.Length - 5) / 2];
-            for (var i = 0; i < command.Length; i++)
+            var coordinates = new double[command.Length / 2];
+            for (var i = 2; i < command.Length; i++)
             {
-                if (double.TryParse(command[i], out coordinates[i]))
+                if (double.TryParse(command[i], out coordinates[i - 2]))
                 {
                 }
             }
