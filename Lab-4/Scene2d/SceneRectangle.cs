231c22ae46e0e64777bb7baa566b1d0068231030
aleksandr_tsvetkov <darksouls2make@gmail.com>
2023-01-18 00:43:50 +0400

    added method for calculating the center of rectangle

--- Lab-4/Scene2d/SceneRectangle.cs
+++ Lab-4/Scene2d/SceneRectangle.cs
@@ -5,5 +5,13 @@ namespace Scene2d
         public ScenePoint Vertex1 { get; set; }
 
         public ScenePoint Vertex2 { get; set; }
+
+        public ScenePoint CalculateTheCenterOfCircumscribedRectangle()
+        {
+            ScenePoint left = Vertex1;
+            ScenePoint right = Vertex2;
+            ScenePoint point = new ScenePoint { X = (right.X + left.X) / 2, Y = (left.Y + right.Y) / 2 };
+            return point;
+        }
     }
 }
