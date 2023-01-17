59931f1007eab2ceb9de6e2b413c43374452cab0
aleksandr_tsvetkov <darksouls2make@gmail.com>
2023-01-18 00:40:52 +0400

    added extension method for ScenePoint

---
+++ Lab-4/Scene2d/Extensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Scene2d
+{
+    using System;
+
+    public static class Extensions
+    {
+        public static ScenePoint RotatePoint(this ScenePoint currentPoint, ScenePoint rotatePoint, double angle)
+        {
+            return new ScenePoint
+            {
+                X = ((currentPoint.X - rotatePoint.X) * Math.Cos(angle)) - ((currentPoint.Y - rotatePoint.Y) * Math.Sin(angle)) + rotatePoint.X,
+                Y = ((currentPoint.X - rotatePoint.X) * Math.Sin(angle)) + ((currentPoint.Y - rotatePoint.Y) * Math.Cos(angle)) + rotatePoint.Y,
+            };
+        }
+    }
+}
