aed248066c1bec2b3b2ad49b4b781aba3f4a1b78
Hokku <darksouls2make@gmail.com>
2023-02-25 21:35:19 +0400

    Added tests for rectangle

--- Lab-4/Scene2d/Scene2d.Tests/RectangleTests.cs
+++ Lab-4/Scene2d/Scene2d.Tests/RectangleTests.cs
@@ -1,18 +1,40 @@
 namespace Scene2d.Tests;
 
+using System.Diagnostics;
 using NUnit.Framework;
+using Scene2d.CommandBuilders;
+using Scene2d.Exceptions;
 using Scene2d.Figures;
 
 public class RectangleTests
 {
-    [TestCase(0.0, 0.0, 2.0, 2.0, 3.0)]
-    public void MoveTest(double p1X, double p1y, double p2x, double p2y, double vector)
+    [TestCase(0.0, 0.0, 2.0, 2.0, 3.0, 3.0)]
+    public void MoveTest_CoordinatesAndVector_IsMoved(double p1X, double p1y, double p2x, double p2y, double vectorx, double vectory)
     {
-        var basedRectangle = new RectangleFigure(new ScenePoint { X = p1X, Y = p1y }, new ScenePoint { X = p2x, Y = p2y });
+        //ARRANGE
         var rectangle = new RectangleFigure(new ScenePoint { X = p1X, Y = p1y }, new ScenePoint { X = p2x, Y = p2y });
-        ScenePoint vectorMove = new ScenePoint { X = vector, Y = vector };
+        ScenePoint vectorMove = new ScenePoint { X = vectorx, Y = vectory };
+        
+        //ACT
         rectangle.Move(vectorMove);
+        var rectangleMovedExcepted =
+            new RectangleFigure(new ScenePoint { X = p1X + vectorx, Y = p1y + vectory }, new ScenePoint { X = p2x + vectorx, Y = p2y + vectory }).CalculateCircumscribingRectangle();
+        var actualMovedRectangle = rectangle.CalculateCircumscribingRectangle();
+        
+        //ASSERT
+        Assert.AreEqual(rectangleMovedExcepted, actualMovedRectangle);
+    }
 
-        Assert.AreEqual(basedRectangle, rectangle);
+    [TestCase("add rectangle t1 (0, 0) (k, k)")]
+    public void AddRectangleTest_stringInput_IsBadFormat(string input)
+    {
+        var rectangle = new AddRectangleCommandBuilder();
+
+        Assert.Throws<BadFormatException>(() => rectangle.AppendLine(input));
+    }
+
+    public void AddRectangleTest_stringInput_IsBadRectanglePointException(string input)
+    {
+        
     }
 }
