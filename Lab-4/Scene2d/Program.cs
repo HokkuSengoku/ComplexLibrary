33bedac47eed7e7e1f172a466a3ee5791571f670
Hokku <darksouls2make@gmail.com>
2023-02-25 21:34:36 +0400

    WIP

--- Lab-4/Scene2d/Program.cs
+++ Lab-4/Scene2d/Program.cs
@@ -6,130 +6,157 @@ namespace Scene2d
     using System.Drawing.Drawing2D;
     using System.Drawing.Imaging;
     using System.IO;
+    using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
     using Scene2d.CommandBuilders;
     using Scene2d.Exceptions;
 
     internal class Program
-    {
-        internal static void Main(string[] args)
         {
-            /*
-             * Main Application Loop
-             */
-
-            Console.WriteLine("Starting scene application...");
-
-            var commandProducer = new CommandProducer();
-            var scene = new Scene();
-
-            bool readCommandsFromFile = args.Length > 0;
-
-            IEnumerable<string> commands = readCommandsFromFile ?
-                ReadCommandsFromFile(args[0]) :
-                ReadCommandsFromUserInput();
-
-            bool drawSceneOnEveryCommand = !readCommandsFromFile;
-
-            foreach (string commandLine in commands)
+            internal static void Main(string[] args)
             {
-                try
+                /*
+                 * Main Application Loop
+                 */
+                Console.WriteLine("Starting scene application...");
+                var commandProducer = new CommandProducer();
+                var scene = new Scene();
+
+                bool readCommandsFromFile = args.Length > 0;
+
+                IEnumerable<string> commands =
+                    readCommandsFromFile ? ReadCommandsFromFile(args[0]) : ReadCommandsFromUserInput();
+
+                bool drawSceneOnEveryCommand = !readCommandsFromFile;
+
+                foreach (string commandLine in commands)
                 {
-                    commandProducer.AppendLine(commandLine);
-
-                    if (commandProducer.IsCommandReady)
+                    try
                     {
-                        var command = commandProducer.GetCommand();
-                        command.Apply(scene);
-
-                        Console.WriteLine(command.FriendlyResultMessage);
-
-                        if (drawSceneOnEveryCommand)
+                        if (commandLine.Split(' ')[0] == "#")
                         {
-                            DrawScene(scene);
+                                   continue;
+                        }
+
+                        commandProducer.AppendLine(commandLine);
+
+                        if (commandProducer.IsCommandReady)
+                        {
+                            var command = commandProducer.GetCommand();
+                            command.Apply(scene);
+
+                            Console.WriteLine(command.FriendlyResultMessage);
+
+                            if (drawSceneOnEveryCommand)
+                            {
+                                DrawScene(scene);
+                            }
                         }
                     }
+                    catch (BadFormatException message)
+                    {
+                        Console.WriteLine($"{message.ExceptionMessage}");
+                    }
+                    catch (BadNameException message)
+                    {
+                        Console.WriteLine($"{message.ExceptionMessage}");
+                    }
+                    catch (NameDoesAlreadyExistException message)
+                    {
+                        Console.WriteLine($"{message.ExceptionMessage}");
+                    }
+                    catch (BadCircleRadiusException message)
+                    {
+                        Console.WriteLine($"{message.ExceptionMessage}");
+                    }
+                    catch (BadRectanglePointException message)
+                    {
+                        Console.WriteLine($"{message.ExceptionMessage}");
+                    }
+                    catch (BadPolygonPointNumberException message)
+                    {
+                        Console.WriteLine($"{message.ExceptionMessage}");
+                    }
+                    catch (BadPolygonPointException message)
+                    {
+                        Console.WriteLine($"{message.ExceptionMessage}");
+                    }
+
+                    /* todo: more exceptions handling here */
                 }
-                catch (BadFormatException)
+
+                if (!drawSceneOnEveryCommand)
                 {
-                    Console.WriteLine("bad format");
+                    DrawScene(scene);
                 }
 
-                /* todo: more exceptions handling here */
+                Console.WriteLine("Commands processing complete.");
             }
 
-            if (!drawSceneOnEveryCommand)
+            private static IEnumerable<string> ReadCommandsFromFile(string input)
             {
-                DrawScene(scene);
+                Console.WriteLine("Reading commands from input file " + input);
+
+                return File.ReadAllLines(input);
             }
 
-            Console.WriteLine("Commands processing complete.");
-        }
-
-        private static IEnumerable<string> ReadCommandsFromFile(string input)
-        {
-            Console.WriteLine("Reading commands from input file " + input);
-
-            return File.ReadAllLines(input);
-        }
-
-        private static IEnumerable<string> ReadCommandsFromUserInput()
-        {
-            while (true)
+            private static IEnumerable<string> ReadCommandsFromUserInput()
             {
-                Console.WriteLine("Enter a command or press Enter to exit");
-                Console.Write("> ");
-
-                string line = Console.ReadLine();
-                if (line == null || line.Trim().Length == 0)
+                while (true)
                 {
-                    break;
+                    Console.WriteLine("Enter a command or press Enter to exit");
+                    Console.Write("> ");
+
+                    string line = Console.ReadLine();
+                    if (line == null || line.Trim().Length == 0)
+                    {
+                        break;
+                    }
+
+                    yield return line;
                 }
-
-                yield return line;
-            }
-        }
-
-        private static void DrawScene(Scene scene)
-        {
-            const string outputFileName = "scene.png";
-
-            if (File.Exists(outputFileName))
-            {
-                File.Delete(outputFileName);
             }
 
-            var area = scene.CalculateSceneCircumscribingRectangle();
-
-            var origin = new ScenePoint
+            private static void DrawScene(Scene scene)
             {
-                X = Math.Min(area.Vertex1.X, area.Vertex2.X),
-                Y = Math.Min(area.Vertex1.Y, area.Vertex2.Y),
-            };
+                const string outputFileName = "scene.png";
 
-            var width = (int)Math.Abs(area.Vertex1.X - area.Vertex2.X) + 1;
-            var height = (int)Math.Abs(area.Vertex1.Y - area.Vertex2.Y) + 1;
-
-            using (Stream output = File.Create(outputFileName))
-            using (Image image = new Bitmap(width, height))
-            using (Graphics drawing = Graphics.FromImage(image))
-            {
-                using (var bg = new SolidBrush(Color.DarkGray))
+                if (File.Exists(outputFileName))
                 {
-                    drawing.FillRectangle(bg, 0, 0, width, height);
+                    File.Delete(outputFileName);
                 }
 
-                drawing.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-                drawing.InterpolationMode = InterpolationMode.HighQualityBilinear;
+                var area = scene.CalculateSceneCircumscribingRectangle();
 
-                foreach (var figure in scene.ListDrawableFigures())
+                var origin = new ScenePoint
                 {
-                    figure.Draw(origin, drawing);
+                    X = Math.Min(area.Vertex1.X, area.Vertex2.X),
+                    Y = Math.Min(area.Vertex1.Y, area.Vertex2.Y),
+                };
+
+                var width = (int)Math.Abs(area.Vertex1.X - area.Vertex2.X) + 1;
+                var height = (int)Math.Abs(area.Vertex1.Y - area.Vertex2.Y) + 1;
+
+                using (Stream output = File.Create(outputFileName))
+                using (Image image = new Bitmap(width, height))
+                using (Graphics drawing = Graphics.FromImage(image))
+                {
+                    using (var bg = new SolidBrush(Color.DarkGray))
+                    {
+                        drawing.FillRectangle(bg, 0, 0, width, height);
+                    }
+
+                    drawing.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                    drawing.InterpolationMode = InterpolationMode.HighQualityBilinear;
+
+                    foreach (var figure in scene.ListDrawableFigures())
+                    {
+                        figure.Draw(origin, drawing);
+                    }
+
+                    image.Save(output, ImageFormat.Png);
                 }
 
-                image.Save(output, ImageFormat.Png);
+                Console.WriteLine("The scene has been saved to " + outputFileName);
             }
-
-            Console.WriteLine("The scene has been saved to " + outputFileName);
         }
     }
-}
