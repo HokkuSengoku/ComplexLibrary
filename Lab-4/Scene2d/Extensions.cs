e7d64d0f221ef1bc7a0a23ec9d31c86cc3d304f7
Hokku <darksouls2make@gmail.com>
2023-02-28 23:31:14 +0400

    WIP

--- Lab-4/Scene2d/Extensions.cs
+++ Lab-4/Scene2d/Extensions.cs
@@ -8,8 +8,10 @@
         {
             return new ScenePoint
             {
-                X = ((currentPoint.X - rotatePoint.X) * Math.Cos(angle)) - ((currentPoint.Y - rotatePoint.Y) * Math.Sin(angle)) + rotatePoint.X,
-                Y = ((currentPoint.X - rotatePoint.X) * Math.Sin(angle)) + ((currentPoint.Y - rotatePoint.Y) * Math.Cos(angle)) + rotatePoint.Y,
+                X = ((currentPoint.X - rotatePoint.X) * Math.Cos(angle)) -
+                    ((currentPoint.Y - rotatePoint.Y) * Math.Sin(angle)) + rotatePoint.X,
+                Y = ((currentPoint.X - rotatePoint.X) * Math.Sin(angle)) +
+                    ((currentPoint.Y - rotatePoint.Y) * Math.Cos(angle)) + rotatePoint.Y,
             };
         }
     }
