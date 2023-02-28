bf425c78f0b62cc59f0b334a3697063bbca97949
Hokku <darksouls2make@gmail.com>
2023-02-28 23:33:05 +0400

    Added Equals override

--- Lab-4/Scene2d/ScenePoint.cs
+++ Lab-4/Scene2d/ScenePoint.cs
@@ -1,5 +1,9 @@
 namespace Scene2d
 {
+    using System;
+    using System.Collections;
+    using System.Drawing;
+
     public struct ScenePoint
     {
         public ScenePoint(double x, double y)
@@ -11,6 +15,16 @@ namespace Scene2d
         public double X { get; set; }
 
         public double Y { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals((ScenePoint)obj);
+        }
+
+        public bool Equals(ScenePoint scenePoint)
+        {
+            return X == scenePoint.X && Y == scenePoint.Y;
+        }
 
         private static void RotatePoint()
         {
