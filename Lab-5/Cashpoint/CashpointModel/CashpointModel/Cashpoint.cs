b02e0073348ed52661fe16fa853ade6ea6ff9c73
Hokku <darksouls2make@gmail.com>
2023-03-03 23:50:05 +0400

    refactor

--- Lab-5/Cashpoint/CashpointModel/CashpointModel/Cashpoint.cs
+++ Lab-5/Cashpoint/CashpointModel/CashpointModel/Cashpoint.cs
@@ -4,11 +4,12 @@ using System.Collections.Generic;
 
 public sealed class Cashpoint
 {
-    private readonly List<uint> _banknotes = new List<uint>();
-    private bool[] _granted = { true };
+    private Dictionary<uint, byte> _banknotes2 = new Dictionary<uint, byte>();
+    private uint[] _granted = { 1 };
 
     private uint _total;
 
+    public uint Count { get; set; }
     public uint Total
     {
         get
@@ -19,42 +20,93 @@ public sealed class Cashpoint
 
     public void AddBanknote(uint value)
     {
-        _banknotes.Add(value);
+        if (!_banknotes2.ContainsKey(value))
+        {
+            _banknotes2[value] = 1;
+        }
+        else
+        {
+            _banknotes2[value]++;
+        }
+        Count++;
         _total += value;
+        CalculateGrants();
+    }
+
+    public void AddBanknote(uint value, int count)
+    {
+        for (var i = 0; i < count; i++)
+        {
+            if (!_banknotes2.ContainsKey(value))
+            {
+                _banknotes2[value] = 1;
+            }
+            else
+            {
+                _banknotes2[value]++;
+            }
+            Count++;
+            _total += value;
+            CalculateGrants();
+        }
     }
 
     public void RemoveBanknote(uint value)
     {
-        if (_banknotes.Remove(value))
+        if (_banknotes2.ContainsKey(value) && _banknotes2[value] != 0)
         {
+            _banknotes2[value]--;
+            Count--;
             _total -= value;
+            CalculateGrants();
+        }
+        else if (_banknotes2.ContainsKey(value)  && _banknotes2[value] == 0)
+        {
+            _banknotes2.Remove(value);
+        }
+    }
+
+    public void RemoveBanknote(uint value, int count)
+    {
+        for (var i = 0; i < count; i++)
+        {
+            if (_banknotes2.ContainsKey(value) && _banknotes2[value] != 0)
+            {
+                _banknotes2[value]--;
+                Count--;
+                _total -= value;
+                CalculateGrants();
+            }
+            else if (_banknotes2.ContainsKey(value)  && _banknotes2[value] == 0)
+            {
+                _banknotes2.Remove(value);
+            }
         }
     }
     
     public bool CanGrant(uint value)
     {
-        CalculateGrants();
-
         if (value > _total)
         {
             return false;
         }
 
-        return _granted[(int)value];
+        return _granted[(int) value] > 0;
     }
 
+    
     private void CalculateGrants()
     {
-        _granted = new bool[_total + 1];
-        _granted[0] = true;
+        _granted = new uint[_total + 1];
+        _granted[0] = 1;
 
-        foreach (var b in _banknotes)
+        foreach (var b in _banknotes2)
         {
             for (var i = (int)_total; i >= 0; i--)
             {
-                if (_granted[i])
+                if (_granted[i] == 1 && _total != 0 && i + b.Key <= _total)
                 {
-                    _granted[i + b] = true;
+                    _granted[i + b.Key] = 1;
                 }
             }
         }
